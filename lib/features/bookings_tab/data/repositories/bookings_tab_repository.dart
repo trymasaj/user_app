d3683884a13b973f20590595ab9ec9388d80e5cb
Abdullah Mohammed <Abdullah.ali@clavisbs.com>
2024-03-27 01:10:03 +0200

    fix booking tab repo

--- lib/features/bookings_tab/data/repositories/bookings_tab_repository.dart
+++ lib/features/bookings_tab/data/repositories/bookings_tab_repository.dart
@@ -3,9 +3,9 @@ import 'package:masaj/features/bookings_tab/data/datasources/bookings_tab_remote
 abstract class BookingsTabRepository {}
 
 class BookingsTabRepositoryImpl implements BookingsTabRepository {
-  final BookingRemoteDataSource _remoteDataSource;
+  final BookingsTabRemoteDataSource _remoteDataSource;
 
   BookingsTabRepositoryImpl({
-    required BookingRemoteDataSource bookings_tabRemoteDataSource,
+    required BookingsTabRemoteDataSource bookings_tabRemoteDataSource,
   }) : _remoteDataSource = bookings_tabRemoteDataSource;
 }
